8adf94015c11d8a8efc7bbe567429ab96d73f8e0
lawankar.anuja08@gmail.com <lawankar.anuja08@gmail.com>
2023-07-06 14:37:29 -0400

    Add fa icon for social media connection

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import './index.css';
 import App from './App';
